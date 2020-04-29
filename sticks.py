df67cf7c62b064ed50918990b1dc0c6c8d99ff67
xdffish <xdffish@qq.com>
2020-04-29 11:44:15 +0800

    First commit

--- sticks.py
+++ sticks.py
@@ -5,7 +5,7 @@ print("There are 21 sticks, you can take 1-4 number of sticks at a time.")
 print("Whoever will take the last stick will loose")
 
 while True:
-    print("Sticks left: " , sticks)
+    print("Sticks left: ", sticks)
     if sticks == 1:
         print("You took the last stick, you loose")
         break
@@ -13,5 +13,5 @@ while True:
     if sticks_taken >= 5 or sticks_taken <= 0:
         print("Wrong choice")
         continue
-    print("Computer took: " , (5 - sticks_taken) , "\n")
+    print("Computer took: ", (5 - sticks_taken), "\n")
     sticks -= 5
